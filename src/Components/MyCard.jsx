f7dda7a64444433d64a7b14b8f89122fbe14eb1b
Martino Brundu <martino.brundu@libero.it>
2024-12-06 16:57:18 +0100

    commit 2

--- src/Components/MyCard.jsx
+++ src/Components/MyCard.jsx
@@ -1,11 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {useState} from 'react'
 
 const URL = 'api.openweathermap.org/data/2.5/forecast?q=Milan,IT&appid=475b77870dc769f01ba14a3245ab0fc4'
 
 
 
-function MyCard() {
+const MyCard = () => {
+   const [meteo, setMeteo] = useState()
+   const [Error, setError]= useState(false)
 
 fetch (URL)
 .then((response) => {
@@ -17,22 +20,12 @@ fetch (URL)
         throw new Error ('la risposta non è ok')
     }
 })
-.then((arrayOfObject) => {
-    return (
-        
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="." />
-          <Card.Body>
-            <Card.Title></Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      );
+.then((data) => {
+    console.log(data)
     
+})
+.catch((err) => {
+    console.log('errore' , err)
 })
 
   
